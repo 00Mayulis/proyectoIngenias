b429a5bc18d5c81f46e807cda82389d034e51d30
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-09-27 21:31:13 -0300

    separe los script para cada html

---
+++ scriptIndex.js
@@ -0,0 +1,4 @@
+// SALUDO - INDEX
+let nombre = prompt("Ingrese su nombre");
+alert("Hola "+nombre+", gracias por visitar nuestro sitio web.Cualquier consulta podes escribirnos por WhatsApp para acceder a una atención personalizada.")
+
